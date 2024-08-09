c219e79e57c89c8e43c3f68748b216b372c8b845
Andrew Le <phu.andrewle@gmail.com>
2024-08-09 01:47:36 -0700

    -Added Logos Bar to About Page -Added Logos to Assets Folder -Need to style margins/gaps/padding for About Section

--- webscraper-react-app/src/pages/AboutPage.jsx
+++ webscraper-react-app/src/pages/AboutPage.jsx
@@ -3,20 +3,56 @@ import { FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
 function AboutPage() {
   return (
     <div
-      className="aboutpage-container flex flex-col gap-8 items-center lg:items-center lg:justify-start px-16 py-4"
+      className="aboutpage-container flex flex-col gap-8 items-center lg:items-center lg:justify-start md:px-16 px-8 py-4"
       id="about"
     >
-      <h1 className="text-5xl font-bold bg-purple-500 text-center mt-20">
+      <h1 className="text-6xl font-extrabold bg-purple-500 text-center mt-20">
         ABOUT BETA BY DREW
       </h1>
+      <h2 className="text-3xl text-slate-200 text-center font-medium italic">
+        {" "}
+        "Built for climbers by climbers"
+      </h2>
       <p className="text-2xl bg-purple-500 text-slate-200 text-center w-full lg:w-[70%]">
         A web scraper built to help you find the best deals for outdoor gear.
-        Curated from trusted retailers and resellers, such as REI, Backcountry,
-        and more.
+        <br />
+        Curated from trusted retailers and resellers.
       </p>
-      <div className="flex flex-col gap-8 justify-center items-center lg:flex-row lg:w-full lg:items-center">
+      <div className="flex flex-wrap items-center justify-center gap-8 px-2">
+        <img
+          src="assets/logos/rei-logo.png"
+          alt="REI"
+          className="aspect-auto w-[100px] h-[80px] lg:w-[160px] lg:h-[140px] object-contain"
+          loading="lazy"
+        />
+        <img
+          src="assets/logos/Backcountry-Logo.png"
+          alt="Backcountry"
+          className="aspect-auto w-[100px] h-[80px] lg:w-[160px] lg:h-[140px] object-contain"
+          loading="lazy"
+        />
+        <img
+          src="assets/logos/moosejaw-logo.png"
+          alt="Moosejaw"
+          className="aspect-auto w-[100px] h-[80px] lg:w-[160px] lg:h-[140px] object-contain"
+          loading="lazy"
+        />
+        <img
+          src="assets/logos/steepandcheap-logo.png"
+          alt="Steep and Cheap"
+          className="aspect-auto w-[100px] h-[80px] lg:w-[160px] lg:h-[140px] object-contain"
+          loading="lazy"
+        />
+        <img
+          src="assets/logos/outdoorgearexchange-logo.png"
+          alt="Outdoor Gear Exchange"
+          className="aspect-auto w-[100px] h-[80px] lg:w-[160px] lg:h-[140px] object-contain"
+          loading="lazy"
+        />
+      </div>
+      <div className="flex flex-col gap-8 justify-center items-center lg:flex-row lg:w-full lg:items-center mt-4">
         <div className="bg-red-400 lg:w-1/2 flex flex-col gap-8 justify-center items-center relative">
-          <h1 className="text-5xl text-slate-200 text-center">
+          <h1 className="text-5xl font-semibold text-slate-200 text-center">
             FROM THE CREATOR
           </h1>
           <p className="text-2xl bg-red-500 text-slate-200 text-center w-[100%]">
@@ -41,7 +77,10 @@ function AboutPage() {
           </div>
         </div>
       </div>
-      <h1 className="text-5xl text-slate-200 text-center" id="contact">
+      <h1
+        className="text-6xl font-extrabold text-slate-200 text-center mt-8 lg:mt-12"
+        id="contact"
+      >
         CONTACT
       </h1>
       <div className="flex flex-row gap-4 mb-8">
