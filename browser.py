f10f1f1183c4fe81cc0e414a518e362cfc71e6f0
Andrew Le <phu.andrewle@gmail.com>
2024-09-19 20:29:41 -0700

    Fixing Webscraper Search Functionality

--- browser.py
+++ browser.py
@@ -45,14 +45,14 @@ class Browser:
         self.prices = []
         try:
             options = webdriver.ChromeOptions()
-            options.add_argument("--headless=new")
+            options.add_argument("--headless")
             options.add_argument('--disable-gpu')
             options.add_argument("--window-size=1920,1080")
             options.add_argument("--start-maximized")
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument("--incognito")
-            options.add_argument("--remote-debugging-pipe")
+            options.add_argument("--remote-debugging-port=9222")
             options.page_load_strategy = 'eager'
             options.add_argument("--disable-extensions")
             options.add_argument("user-agent=some_user_agent")
