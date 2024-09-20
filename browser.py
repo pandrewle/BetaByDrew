ab91464f75818d60f1e577b6b6c2929a487e4793
Andrew Le <phu.andrewle@gmail.com>
2024-09-19 20:54:53 -0700

    Fixing Webscraper Search Functionality

--- browser.py
+++ browser.py
@@ -45,11 +45,12 @@ class Browser:
         self.prices = []
         try:
             options = webdriver.ChromeOptions()
-            options.add_argument("--headless")
+            options.add_argument('--no-sandbox')
+            options.add_argument("--headless=new")
+            options.add_argument('--ignore-certificate-errors')
             options.add_argument('--disable-gpu')
             options.add_argument("--window-size=1920,1080")
             options.add_argument("--start-maximized")
-            options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument("--incognito")
             options.add_argument("--remote-debugging-port=9222")
@@ -68,10 +69,10 @@ class Browser:
             logger.debug(f"Chrome binary location set to: {chrome_bin_path}")
             logger.debug(f"Chromedriver path set to: {chromedriver_path}")
 
-            service = ChromeService(executable_path=chromedriver_path)
-            logger.debug("Setting up Chrome service with webdriver_manager")
+            # service = ChromeService(executable_path=chromedriver_path)
+            # logger.debug("Setting up Chrome service with webdriver_manager")
 
-            self.driver = webdriver.Chrome(service=service, options=options)
+            self.driver = webdriver.Chrome(options=options)
             logger.debug("Initializing WebDriver with Chrome options")
 
             self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
