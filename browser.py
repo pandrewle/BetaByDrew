82b412d505a8c11bce8ecaad1a1f7da0c8fd75c6
Andrew Le <phu.andrewle@gmail.com>
2024-09-22 13:29:27 -0700

    Fixing Webscraper Search Functionality

--- browser.py
+++ browser.py
@@ -46,7 +46,7 @@ class Browser:
         try:
             options = webdriver.ChromeOptions()
             options.add_argument('--no-sandbox')
-            # options.add_argument("--headless=new")
+            options.add_argument("--headless=new")
             options.add_argument('--ignore-certificate-errors')
             options.add_argument('--disable-gpu')
             options.add_argument("--window-size=1920,1080")
